7308dff479bb5118f52f7cb0b4bf6c9867ef7b5e
leandro98av <leandro98av@gmail.com>
2021-03-17 17:03:29 -0300

    .

--- site_La_Bodega/controller/sessionController.js
+++ site_La_Bodega/controller/sessionController.js
@@ -39,7 +39,7 @@ module.exports = {
              }); */
 
 /*             const passHash = bcrypt.hashSync(pass, 12)
- */            db.User.create({
+ */            db.Users.create({
                 name,
                 apellido: lastName,
                 direccion: address,
@@ -51,34 +51,34 @@ module.exports = {
                 .then(() => res.redirect('/users/login'))
                 .catch(error => res.send(error))
         }
-            /* let newUser = {
-                id: +lastID + 1,
-                name,
-                lastName,
-                email,
-                address,
-                gender,
-                dateOfBirth,
-                pass: passHash,
-                img: req.files[0].filename,
-                Admin : false
-            }
-            users.push(newUser)
-            setUsers(users);
+        /* let newUser = {
+            id: +lastID + 1,
+            name,
+            lastName,
+            email,
+            address,
+            gender,
+            dateOfBirth,
+            pass: passHash,
+            img: req.files[0].filename,
+            Admin : false
+        }
+        users.push(newUser)
+        setUsers(users);
 
-            return res.redirect('login')
-        } */
-        },
-        processLogin: (req, res) => {
-            let errores = validationResult(req);
-            const { email, pass, recordar } = req.body;
+        return res.redirect('login')
+    } */
+    },
+    processLogin: (req, res) => {
+        let errores = validationResult(req);
+        const { email, pass, recordar } = req.body;
 
-            if (!errores.isEmpty()) {
-                return res.render('users/login', {
-                    errores: errores.mapped(),
-                    data: req.body
-                })
-            } else {
+        if (!errores.isEmpty()) {
+            return res.render('users/login', {
+                errores: errores.mapped(),
+                data: req.body
+            })
+        } else {
 
 
             /*     let result = users.find(user => user.email === email);
@@ -115,55 +115,55 @@ module.exports = {
                 })
             } */
             db.Users.findOne({
-                where:{
-                     email
+                where: {
+                    email
                 }
             })
-            .then((user)=>{
-                if(user){
-                    if(bcrypt.compareSync(pass.trim(), user.pass)){
-                        req.session.user = {
-                            id : user.id,
-                            name : user.name,
-                            lastName: user.apellido,
-                            email: user.email,
-                            img: user.avatar,   
+                .then((user) => {
+                    if (user) {
+                        if (bcrypt.compareSync(pass.trim(), user.pass)) {
+                            req.session.user = {
+                                id: user.id,
+                                name: user.name,
+                                lastName: user.apellido,
+                                email: user.email,
+                                img: user.avatar,
+                            }
+                            if (typeof recordar != 'undefined') {
+                                res.cookie('LaBodega', req.session.user, {
+                                    maxAge: 1000 * 60 * 60 * 24 * 100000
+                                })
+                            }
+                            return res.redirect('profile')
                         }
-                        if(typeof recordar != 'undefined'){
-                            res.cookie('LaBodega', req.session.user,{
-                                maxAge :  1000 * 60 * 60 * 24 * 100000
-                            })
-                        }
-                        return res.redirect('profile')
-                    } 
-                }
-                res.render('users/login', {
-                    errores: 
+                    }
+                    res.render('users/login', {
+                        errores:
                         {
                             pass: {
-                                msg : 'Credenciales inválidas'
-                            } 
+                                msg: 'Credenciales inválidas'
+                            }
                         },
-                    data : req.body    
-                    
+                        data: req.body
+
+                    })
                 })
-            })
-            .catch((error)=>{
-                res.send(error)
-            })
+                .catch((error) => {
+                    res.send(error)
+                })
         }
     },
-            profile: (req, res) => {
-                res.render('users/profile')
-            },
-                logout: (req, res) => {
-                    req.session.destroy();
-                    if (req.cookies.LaBodega) {
-                        res.cookie('LaBodega', '', { maxAge: -1 })
-                    }
-                    res.redirect('/')
-                }
+    profile: (req, res) => {
+        res.render('users/profile')
+    },
+    logout: (req, res) => {
+        req.session.destroy();
+        if (req.cookies.LaBodega) {
+            res.cookie('LaBodega', '', { maxAge: -1 })
+        }
+        res.redirect('/')
     }
+}
 
 
 
