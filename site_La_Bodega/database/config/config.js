b085e646c59ec892f8128dadd175e4ed99e54090
leandro98av <leandro98av@gmail.com>
2021-03-17 19:39:32 -0300

    cambios

--- site_La_Bodega/database/config/config.js
+++ site_La_Bodega/database/config/config.js
@@ -10,14 +10,14 @@ module.exports = {
   },
   test: {
     username: "root",
-    password: null,
+    password: process.env.DB_PASSWORD,
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
-    password: null,
+    password: process.env.DB_PASSWORD,
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql",
