27c15a8cadae69ffad7045d95db1dee03d647a38
leandro98av <leandro98av@gmail.com>
2021-03-22 16:22:05 -0300

    cambios

--- site_La_Bodega/controller/adminController.js
+++ site_La_Bodega/controller/adminController.js
@@ -18,30 +18,8 @@ module.exports = {
     },
     storeWine: (req, res, next) => {
 
-        /* let lastID = 1;
-        wines.forEach(wine => {
-            if (wine.id > lastID) {
-                lastID = wine.id
-            }
-        }); */
-
         const { name, detail, price_box, price_unit,/*  type, */ variety, year, time, color, origin } = req.body;
 
-        /* let wine = {
-            id: lastID + 1,
-            name,
-            detail,
-            price_box,
-            price_unit,
-            type, variety,
-            year,
-            time,
-            color,
-            origin,
-            img: req.files[0].filename
-
-            
-        } */
         
         let img = req.files[0].filename;
 
@@ -64,9 +42,7 @@ module.exports = {
             })
             .catch((error) => res.send(error));
 
-        /*   wines.push(wine);
-          setWines(wines);
-          res.redirect('/admin/products'); */
+    
     },
     editWine: (req, res) => {
 
@@ -79,10 +55,7 @@ module.exports = {
             .catch((error) => {
                 res.send(error)
             })
-        /*   const wine = wines.find(wine => wine.id === +req.params.id);
-          res.render('admin/productEdit', {
-              wine
-          }) */
+        
     },
     updateWine: (req, res, next) => {
         const { name, detail, price_box, price_unit, /* type, */ variety, year, time, color, origin } = req.body;
@@ -117,33 +90,6 @@ module.exports = {
             .catch((error) => {
                 res.send(error)
             })
-
-
-        /*    
-        wines.forEach(wine => {
-            if(wine.id === +req.params.id){
-    
-                if(fs.existsSync(path.join('public','images','botellas',wine.img))){
-                    fs.unlinkSync(path.join('public','images','botellas',wine.img));
-                }
-    
-                wine.id = +req.params.id;
-                wine.name=name, 
-                wine.detail=detail, 
-                wine.price_box=price_box,
-                wine.price_unit=price_unit, 
-                wine.type=type, 
-                wine.variety=variety, 
-                wine.year=year, 
-                wine.time=time,
-                wine.color=color,
-                wine.origin=origin,
-                wine.img=img
-            }
-        });
-    
-        setWines(wines);
-        res.redirect('/admin/products'); */
     },
     deleteWine: (req, res) => {
         db.Products.destroy({
@@ -158,20 +104,5 @@ module.exports = {
                 res.send(error)
             })
     }
-
-    /*  wines.forEach(wine => {
-         if(wine.id === +req.params.id){
-
-             if(fs.existsSync(path.join('public','images','botellas',wine.img))){
-                 fs.unlinkSync(path.join('public','images','botellas',wine.img));
-             }
-             let eliminar = wines.indexOf(wine);
-             wines.splice(eliminar,1);
-         }
-     });
-
-     setWines(wines);
-
-     res.redirect('/admin/products'); */
 
 }
