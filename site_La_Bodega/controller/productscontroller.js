19ed9b9426a26046e9f76b59ae86361dbd777247
Cristian <cristiannicolas32@hotmail.com>
2021-04-06 02:05:33 -0300

    arreglo la vista de contacto

--- site_La_Bodega/controller/productscontroller.js
+++ site_La_Bodega/controller/productscontroller.js
@@ -1,116 +1,97 @@
-const {getWines} = require('../data/products');
-const db = require('../database/models')
+const { getWines } = require("../data/products");
+const db = require("../database/models");
 
 const products = getWines();
-const {Op, where} = require('sequelize');
-const { random } = require('faker');
-const Sequelize = require('sequelize')
+const { Op, where } = require("sequelize");
+const { random } = require("faker");
+const Sequelize = require("sequelize");
 
-
-module.exports={
-    list: (req,res)=>{
-        let tintos = db.Category.findAll(
-            {
-                where: {
-                    id: 1
+module.exports = {
+    list: (req, res) => {
+        let tintos = db.Category.findAll({
+            where: {
+                id: 1,
+            },
+            include: [
+                {
+                    association: "vinos",
                 },
-                include:[
-                    {
-                        association : 'vinos'
-                    }
-                ]
-            }
-        )
-        let blancos = db.Category.findAll(
-            {
-                where: {
-                    id: 2
-                },
-                include:[
-                    {
-                        association : 'vinos'
-                    }
-                ]
-            }
-        )
-        let espumantes = db.Category.findAll(
-            {
-                where: {
-                    id: 3
-                },
-                include:[
-                    {
-                        association : 'vinos'
-                    }
-                ]
-            }
-        )
-        Promise.all([tintos,blancos,espumantes])
-        .then(([tintos,blancos,espumantes]) => {
-            
-            res.render('products',{
-                tintos:tintos[0].vinos,
-                blancos:blancos[0].vinos,
-                espumantes:espumantes[0].vinos
-            })
-        })  
-        .catch(error => res.send(error))
-        
-    },
-    detail: (req, res)=>{
-        const {id} = req.params
-        let product = db.Product.findOne(
-            {
-                where: {
-                    id: +id
-                },
-                include:[
-                    {
-                        association : 'category'
-                    },
-                    {
-                        association : 'color'
-                    }
-                ]
-            }
-        )
-        let aleatorio = db.Product.findAll({
-            order : [
-                [Sequelize.literal('RAND()')]
             ],
-            limit : 4
-            
-        })
-        
-        Promise.all([product,aleatorio])
-        .then(([product,aleatorio]) =>{
-            res.render('detail',{
-                product:product,
-                aleatorio:aleatorio
+        });
+        let blancos = db.Category.findAll({
+            where: {
+                id: 2,
+            },
+            include: [
+                {
+                    association: "vinos",
+                },
+            ],
+        });
+        let espumantes = db.Category.findAll({
+            where: {
+                id: 3,
+            },
+            include: [
+                {
+                    association: "vinos",
+                },
+            ],
+        });
+        Promise.all([tintos, blancos, espumantes])
+            .then(([tintos, blancos, espumantes]) => {
+                res.render("products", {
+                    tintos: tintos[0].vinos,
+                    blancos: blancos[0].vinos,
+                    espumantes: espumantes[0].vinos,
+                });
             })
-        })
+            .catch((error) => res.send(error));
     },
-    cart:(req,res)=>{
-        res.render('cart')        
+    detail: (req, res) => {
+        const { id } = req.params;
+        let product = db.Product.findOne({
+            where: {
+                id: +id,
+            },
+            include: [
+                {
+                    association: "category",
+                },
+                {
+                    association: "color",
+                },
+            ],
+        });
+        let aleatorio = db.Product.findAll({
+            order: [[Sequelize.literal("RAND()")]],
+            limit: 4,
+        });
+
+        Promise.all([product, aleatorio]).then(([product, aleatorio]) => {
+            res.render("detail", {
+                product: product,
+                aleatorio: aleatorio,
+            });
+        });
+    },
+    cart: (req, res) => {
+        res.render("cart");
     },
     search: (req, res) => {
         productos = db.Product.findAll({
             where: {
-             [Op.or] : [
-                { 'name' : { [Op.substring] : `%${req.query.search}%`} },
-                { 'variety' : { [Op.substring] : `%${req.query.search}%` }}
-             ]
-            }
-        })
-        Promise.all([productos])
-        .then(([productos]) => {
-            res.render('search',{
+                [Op.or]: [
+                    { name: { [Op.substring]: `%${req.query.search}%` } },
+                    { variety: { [Op.substring]: `%${req.query.search}%` } },
+                ],
+            },
+        });
+        Promise.all([productos]).then(([productos]) => {
+            res.render("search", {
                 productos,
-                title : "Resultado de la busqueda"
-            })
-        })
-        
+                title: "Resultado de la busqueda",
+            });
+        });
     },
-    
-    
-}
+};
